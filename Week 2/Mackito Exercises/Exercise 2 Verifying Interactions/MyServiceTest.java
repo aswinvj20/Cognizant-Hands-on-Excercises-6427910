bf89e7f65af80026132d02d5dce1f1b5dc528973
aswinvj20 <aswinaswin.k654321@gmail.com>
2025-06-29 22:47:43 +0530

    MyServiceTest.java

---
+++ Week 2/Mackito Exercises/Exercise 2 Verifying Interactions/MyServiceTest.java
@@ -0,0 +1,15 @@
+
+public class MyServiceTest {
+    public static void main(String[] args) {
+        MockExternalApi mockApi = new MockExternalApi();
+
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+
+        if (mockApi.wasGetDataCalled()) {
+            System.out.println("Test Passed: getData() was called.");
+        } else {
+            System.out.println("Test Failed: getData() was NOT called.");
+        }
+    }
+}
