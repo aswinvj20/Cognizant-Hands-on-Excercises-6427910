ecdf7b4c91f5dd72b90ba20f052283564cf410f1
aswinvj20 <aswinaswin.k654321@gmail.com>
2025-06-29 22:46:26 +0530

    MockExternalApi.java

---
+++ Week 2/Mackito Exercises/Exercise 2 Verifying Interactions/MockExternalApi.java
@@ -0,0 +1,14 @@
+
+public class MockExternalApi implements ExternalApi {
+    boolean getDataCalled = false;
+
+    @Override
+    public void getData() {
+        getDataCalled = true;
+        System.out.println("getData() was called");
+    }
+
+    public boolean wasGetDataCalled() {
+        return getDataCalled;
+    }
+}
