c3a9dea54e0c05ea15bff9ea4316d27e972ef303
aswinvj20 <aswinaswin.k654321@gmail.com>
2025-07-05 12:05:09 +0530

    Create BookRepository.java

---
+++ Week 3/SpringCore and Maven/Exercise 1:Configuring a Basic Spring Application/BookRepository.java
@@ -0,0 +1,7 @@
+
+package com.library.repository;
+public class BookRepository{
+    public void displayBook(){
+        System.out.println("Book Details: Title: Wings Of Fire, Author: Abdul Kalam, ISBN: 20256");
+    }
+}
