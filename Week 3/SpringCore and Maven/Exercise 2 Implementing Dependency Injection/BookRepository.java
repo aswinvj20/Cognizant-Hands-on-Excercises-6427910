2b6abc3bd5276e6ed9d601d07b51a08d72a62b33
aswinvj20 <aswinaswin.k654321@gmail.com>
2025-07-05 12:09:35 +0530

    Create BookRepository.java

---
+++ Week 3/SpringCore and Maven/Exercise 2 Implementing Dependency Injection/BookRepository.java
@@ -0,0 +1,7 @@
+
+package com.library.repository;
+public class BookRepository{
+    public String getBookDetails(){
+        return "Title: Meg: A Novel of Deep Terror, Author: Steve Alten, ISBN: 7894561";
+    }
+}
